471efc19503c3d49e7a4bcf34b4ec25d0ec19b1e
dreckmal <dreckmal@gmail.com>
2025-06-24 22:08:36 -0400

    added player drawing. In the middle of checking turn capabilites.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
+from player import *
 
 
 def main():
@@ -12,14 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    my_pc = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_pc.draw(screen)
+        my_pc.update(dt)
         pygame.display.flip()
-
+      
         dt = clock.tick(60) / 1000
         
         
